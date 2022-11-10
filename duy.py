00d47cda6b17f85aa6599c2e43710dec24f29944
Duy IT <duyekko189@gmail.com>
2022-11-10 08:46:05 +0700

    first commit

---
+++ duy.py
@@ -0,0 +1,26 @@
+import numpy as np 
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+#### RANDM DATA
+
+
+A= [2,5,7,9,11,16,19,23,22,29,29,35,37,40,46]
+b= [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+print("leduyratdeptria")
+#VISUALIZE DATA
+
+
+plt.plot(A,b,'ro')
+ 
+
+
+
+plt.show()
+
+
+
+
+
